3e89fa03bed27f1f5f1168a7bdf4f6b36cdc3172
Nikhila L S <163385096+nklaahhh@users.noreply.github.com>
2024-09-19 15:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,29 +2,11 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-import subprocess
-import streamlit as st
-
-def upgrade_pip():
-    with st.spinner("Upgrading pip..."):
-        # Upgrade pip using subprocess to run the command
-        result = subprocess.run(["python", "-m", "pip", "install", "--upgrade", "pip"], capture_output=True, text=True)
-        
-        # Check for errors in the result
-        if result.returncode == 0:
-            st.success("Pip has been upgraded successfully!")
-        else:
-            st.error(f"Failed to upgrade pip:\n{result.stderr}")
-
-# Button in Streamlit to trigger the pip upgrade
-if st.button("Upgrade pip"):
-    upgrade_pip()
-
 
 
 
 def load_model():
-    model = tf.keras.models.load_model(r"C:/Users/MY PC\Downloads\deeplearning/CNN/result1/model/cnn_tumor.h5")
+    model = tf.keras.models.load_model('cnn_tumor2.h5')
     return model
 
 model = load_model()
